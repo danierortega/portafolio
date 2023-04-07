af3af32fead453f4a66e077c63bd1645a5655af1
danierRestrepo <danierr.ortega@udea.edu.co>
2023-04-07 18:05:45 -0500

    se tienen listas las sections cabecera, menu, title, sobre mi, skills, formacion y se empezo section experiencia

---
+++ script.js
@@ -0,0 +1,38 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+});
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
+
+function validarDatos(){
+    window.event.preventDefault()
+     
+    if (document.form.nombre.value=="" ) {
+        alert("Campo nombre es obligatorio")  
+        document.form.nombre.focus()
+    
+    }else if (document.form.email.value=="") {
+        alert("Campo e-mail es obligatorio")
+        document.form.email.focus() 
+
+    }else if (document.form.asunto.value=="" ) {
+        alert("Campo Asunto es obligatorio")
+        document.form.asunto.focus() 
+
+    }else if (document.form.mensaje.value=="" || document.form.mensaje.value.length <= 50 ){
+        alert("Campo Mensaje es obligatorio y debe contener máximo 50 carateres") 
+        document.form.mensaje.focus()
+      
+    } else if (document.form.email.value.indexOf('@')==-1 ||
+    document.form.email.value.indexOf('.')==-1 ) {
+        alert("e-mail inválido")
+    }
+   
+}
+    document.querySelector('form').addEventListener('submit',validarDatos)
